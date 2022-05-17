3ef8958ab41d78ffd5a6191e70c7961a4d0201c5
MINHWAISGOOD <minhwaisgood@naver.com>
2022-05-17 11:58:00 +0900

    5/17 2

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,61 +1,73 @@
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import Menu from './Menu';
+import { useState, useRef, useEffect } from 'react';
 
 const path = process.env.PUBLIC_URL;
 
 function Header(props) {
+	const menu = useRef(null);
 	const active = { color: 'gray' };
+	const [toggle, setToggle] = useState(false);
+
+	useEffect(() => {
+		toggle ? menu.current.open() : menu.current.close();
+	}, [toggle]);
 
 	return (
-		<header className={props.type}>
-			<div className='inner'>
-				<h1>
-					<NavLink exact to='/'>
-						<div className='pic'>
-							<img src={`${path}/img/logo.png`} />
-						</div>
-					</NavLink>
-				</h1>
+		<>
+			<header className={props.type}>
+				<div className='inner'>
+					<h1>
+						<NavLink activeStyle={active} exact to='/'>
+							<div className='pic'>
+								<img src={`${path}/img/logo.png`} />
+							</div>
+						</NavLink>
+					</h1>
 
-				<div id='gnb'>
-					<li>
-						<NavLink activeStyle={active} to='/about'>
-							About
-						</NavLink>
-					</li>
-					<li>
-						<NavLink activeStyle={active} to='/community'>
-							Community
-						</NavLink>
-					</li>
-					<li>
-						<NavLink activeStyle={active} to='/gallery'>
-							Gallery
-						</NavLink>
-					</li>
-					<li>
-						<NavLink activeStyle={active} to='/youtube'>
-							Youtube
-						</NavLink>
-					</li>
-					<li>
-						<NavLink activeStyle={active} to='/location'>
-							Location
-						</NavLink>
-					</li>
-					<li>
-						<NavLink activeStyle={active} to='/join'>
-							Join
-						</NavLink>
-					</li>
+					<div id='gnb'>
+						<li>
+							<NavLink activeStyle={active} to='/about'>
+								About
+							</NavLink>
+						</li>
+						<li>
+							<NavLink activeStyle={active} to='/community'>
+								Community
+							</NavLink>
+						</li>
+						<li>
+							<NavLink activeStyle={active} to='/gallery'>
+								Gallery
+							</NavLink>
+						</li>
+						<li>
+							<NavLink activeStyle={active} to='/youtube'>
+								Youtube
+							</NavLink>
+						</li>
+						<li>
+							<NavLink activeStyle={active} to='/location'>
+								Location
+							</NavLink>
+						</li>
+						<li>
+							<NavLink activeStyle={active} to='/join'>
+								Join
+							</NavLink>
+						</li>
+					</div>
+
+					<a className='menuMo'>
+						<FontAwesomeIcon icon={faBars} onClick={() => setToggle(!toggle)} />
+					</a>
 				</div>
+			</header>
 
-				<a href='#' className='menuMo'>
-					<FontAwesomeIcon icon={faBars} />
-				</a>
-			</div>
-		</header>
+			<Menu ref={menu} toggle={toggle} setToggle={setToggle} />
+		</>
 	);
 }
 
---
+++ src/components/common/Menu.js
@@ -0,0 +1,60 @@
+import { NavLink } from 'react-router-dom';
+import { forwardRef, useImperativeHandle, useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const Menu = forwardRef((props, ref) => {
+	const [open, setOpen] = useState(false);
+
+	useImperativeHandle(ref, () => {
+		return {
+			open: () => setOpen(true),
+			close: () => setOpen(false),
+		};
+	});
+	return (
+		<AnimatePresence>
+			{open && (
+				<motion.nav
+					initial={{ x: -280, opacity: 0 }}
+					animate={{
+						x: 0,
+						opacity: 1,
+						transition: { type: 'spring', bounce: 0 },
+					}}
+					exit={{ x: -280, opacity: 0 }}
+					onClick={() => {
+						setOpen(false);
+						props.setToggle(!props.toggle);
+					}}>
+					<h1>
+						<NavLink exact to='/'>
+							LOGO
+						</NavLink>
+					</h1>
+					<ul id='gnb'>
+						<li>
+							<NavLink to='/about'>About</NavLink>
+						</li>
+						<li>
+							<NavLink to='/community'>Commnunity</NavLink>
+						</li>
+						<li>
+							<NavLink to='/gallery'>Gallery</NavLink>
+						</li>
+						<li>
+							<NavLink to='/youtube'>Youtube</NavLink>
+						</li>
+						<li>
+							<NavLink to='/location'>Location</NavLink>
+						</li>
+						<li>
+							<NavLink to='/join'>Join</NavLink>
+						</li>
+					</ul>
+				</motion.nav>
+			)}
+		</AnimatePresence>
+	);
+});
+
+export default Menu;
